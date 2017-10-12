a4bb2e6bc0c00f607dd15c1197000070bd02fee1
Your Name <neeraj@systologysoftware.com>
2017-10-13 00:26:51 +0530

    - working build and tests

---
+++ Example/Tests/NCoreConfigSpec.swift
@@ -0,0 +1,25 @@
+//
+//  NCoreConfigSpec.swift
+//  NCore_Tests
+//
+//  Created by Systology Software Pvt Ltd on 13/10/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NCore
+@testable import NCore
+
+class NCoreConfigSpec: QuickSpec {
+    override func spec() {
+        describe("NCoreConfig Initialization") {
+            let ncore = NCoreConfig()
+            it("should get initialized and not be nil") {
+                expect(ncore).toNot(beNil())
+                
+            }
+        }
+    }
+}
+
---
+++ NCore/Classes/NCoreConfig.swift
@@ -0,0 +1,12 @@
+//
+//  NCoreConfig.swift
+//  NCore
+//
+//  Created by Systology Software Pvt Ltd on 13/10/17.
+//
+
+import Foundation
+
+class NCoreConfig {
+    
+}
