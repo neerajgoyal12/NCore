d270812f141d6d8d89abb9116ed52de784a1e5a2
Your Name <neeraj@systologysoftware.com>
2017-10-12 23:57:13 +0530

    Initial commit

---
+++ Example/NCore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NCore
+//
+//  Created by neerajgoyal12 on 10/12/2017.
+//  Copyright (c) 2017 neerajgoyal12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
