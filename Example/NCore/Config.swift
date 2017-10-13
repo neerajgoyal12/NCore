c5f333dba1f83ff85ac1cd263604e421b883c106
Neeraj Goyal <neerajgoyal12@gmail.com>
2017-10-14 02:27:53 +0530

    - public static let can not be overridden

--- Example/NCore/Config.swift
+++ Example/NCore/Config.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 class Config {
-    static let sharedConfig = Config()
+    public static let sharedConfig = Config()
     var someString: String?
     required internal init() {
         someString = "Config1"
